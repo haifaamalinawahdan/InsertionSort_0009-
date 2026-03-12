25baadd03fad34aab9f4c69ff88d658befbf4167
haifaamalinawahdan <haifanew3@gmail.com>
2026-03-12 15:09:19 +0700

    int main

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -21,9 +21,9 @@ void input () //Procedure input
         }
     }
     cout << endl;
-    cout << "=====================================";
-    cout << "=======Masukan Element Array=========";
-    cout << "=====================================";
+    cout << "=====================================" << endl;
+    cout << "=======Masukan Element Array=========" << endl;
+    cout << "=====================================" << endl;
 
     for (int i = 0; i<n; i++)
     {
@@ -60,11 +60,19 @@ void display()
     cout << endl;
     cout << "\n=================";
     cout << "\nElement Array" << endl;
-    cout << "\n=================";
+    cout << "\n=================" << endl;
 
     for(int j = 0; j < n; j++)
     {
         cout << arr[j] << endl;
     }
     cout << endl;
+}
+
+int main()
+{
+    input();
+    insertionsort();
+    display();
+    system("pause");
 }
