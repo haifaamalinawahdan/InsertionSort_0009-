3ab49e368a421446aa0ee0d182d47ec1ed35ab44
haifaamalinawahdan <haifanew3@gmail.com>
2026-03-12 14:52:32 +0700

    Cout & cin data array ke-

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -24,4 +24,10 @@ void input () //Procedure input
     cout << "=====================================";
     cout << "=======Masukan Element Array=========";
     cout << "=====================================";
+
+    for (int i = 0; i<n; i++)
+    {
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
 }
