3648dd741055237613b928b2207f569eb0df8234
haifaamalinawahdan <haifanew3@gmail.com>
2026-03-12 14:41:36 +0700

    Prosedur input data pada array

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -3,3 +3,21 @@ using namespace std;
 
 int arr[20];    //membuat array dengan panjang data 20
 int n;           //membuat variable inputan n
+
+void input () //Procedure input
+{
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+}
